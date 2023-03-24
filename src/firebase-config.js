9f9c0cfb07c13eed01beff855f23e9a73df9d963
Neilly28 <neilpilarca@gmail.com>
2023-03-24 22:33:06 +0100

    add deployment

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,19 +2,30 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
+// };
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: "AIzaSyAKquKC6yqPdydz0JKHIOR283dneLzNKus",
+  authDomain: "fireblog-c63ae.firebaseapp.com",
+  projectId: "fireblog-c63ae",
+  storageBucket: "fireblog-c63ae.appspot.com",
+  messagingSenderId: "2176795904",
+  appId: "1:2176795904:web:e02e6aeebd8d249c529fe5",
 };
 
 // Initialize Firebase
